29caeeaf01ede4137fa5aac05e682de52f83d56f
Ivan <ivan.rachev@outlook.com>
2023-03-08 20:15:57 +0200

    small changes

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
 const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const clientId = process.env['clientid'];
+const guildId = process.env['testguildid'];
+const commands = [];
 
-const commands = {
-  name: `hey`,
-  description: `Reply with hey`,
-};
 const rest = new REST({ verison: '10' }).setToken(process.env.TOKEN);
 (async () => {
   try {
     console.log('Registering cmnds...');
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
       { body: commands }
     );
     console.log('Cmnds registered');
